8b78f4285351681155b40ac1878845da255bf768
Didier Peran Ganthier <didierganthierperan@gmail.com>
2024-12-31 10:16:07 -0500

    Add routes to get all todos and get a single todo

--- server/index.js
+++ server/index.js
@@ -25,8 +25,30 @@ app.post('/todos', async (req, res) => {
 })
 
 // get all todos
+app.get('/todos', async (req, res) => {
+    try {
+        const allTodos = await pool.query(
+            "SELECT * FROM todo"
+        )
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 // get a todo
+app.get('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query(
+            "SELECT * FROM todo WHERE todo_id = $1",
+            [id]
+        )
+        res.json(todo.rows[0])
+    } catch (error) {
+        
+    }
+})
 
 // update a todo
 
